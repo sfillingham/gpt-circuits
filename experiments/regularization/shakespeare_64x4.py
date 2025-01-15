35fdc832b2d9142aa0cef98c9e668eb0503ef5bc
Peter Lai <peterjlai@gmail.com>
2025-01-15 03:26:00 -0800

    updating parameter sweeps

--- experiments/regularization/shakespeare_64x4.py
+++ experiments/regularization/shakespeare_64x4.py
@@ -5,8 +5,10 @@ $ python -m experiments.regularization.shakespeare_64x4 --experiment=0
 """
 
 import argparse
+import json
 from pathlib import Path
 
+import pandas as pd
 import torch
 
 from config import TrainingConfig
@@ -104,21 +106,25 @@ if __name__ == "__main__":
     args = parse_args()
 
     match args.experiment:
-        # Train a GPT model
         case 0:
+            """
+            Train a GPT model
+            """
             # Load configuration
             config = gpt_training_options["shakespeare_64x4"]
-            config.name = "experiments/shakespeare_64x4.normal"
+            config.name = "experiments/regularization/shakespeare_64x4.normal"
 
             # Initialize trainer
             trainer = GPTTrainer(config)
             trainer.train()
 
-        # Train a GPT model using SAE regularization
         case 1:
+            """
+            Train a GPT model using SAE regularization
+            """
             # Load configuration
             config = sae_training_options["train.a.gated_v2x8.shakespeare_64x4"]
-            config.name = "experiments/shakespeare_64x4.regularized"
+            config.name = "experiments/regularization/shakespeare_64x4.regularized"
             config.sae_config.n_features = tuple(64 * n for n in (8, 8, 32, 64, 64))
             config.loss_coefficients.l1 = (0.5, 0.5, 1.0, 3.5, 3.5)
             config.max_steps = 15000
@@ -127,32 +133,97 @@ if __name__ == "__main__":
             trainer = RegularizationTrainer(config, torch.tensor(100.0))
             trainer.train()
 
-        # Train SAE layers for a GPT model
         case 2:
+            """
+            Train SAE layers for a GPT model
+            """
             # Sweep loss coefficients
             # The following coefficients yield l0s ~ 10.0: (0.4, 0.8, 7.5, 23.0, 40.0)
             parameter_sets = []
             starting_coefficients = (0.1, 0.2, 1.0, 8.0, 14.0)
             ending_coefficients = (2.0, 4.0, 20.0, 65.0, 90.0)
-            sweep_training_parameters(
-                name_prefix="experiments/sae.shakespeare_64x4.normal",
-                load_from=TrainingConfig.checkpoints_dir / "experiments/shakespeare_64x4.normal",
-                starting_from=starting_coefficients,
-                ending_with=ending_coefficients,
-                steps=24,
-            )
+            num_sweeps = 5
+            for i in range(num_sweeps):
+                print(f"Starting parameter sweep {i}/{num_sweeps}")
+                sweep_training_parameters(
+                    name_prefix="experiments/regularization/sae.shakespeare_64x4.normal",
+                    load_from=TrainingConfig.checkpoints_dir / "experiments/regularization/shakespeare_64x4.normal",
+                    starting_from=starting_coefficients,
+                    ending_with=ending_coefficients,
+                    steps=24,
+                )
 
-        # Train SAE layers for a GPT model created using SAE regularization
         case 3:
+            """
+            Train SAE layers for a GPT model created using SAE regularization
+            """
             # Sweep loss coefficients
             # The following coefficients yield l0s ~ 10.0: (0.15, 0.33, 1.5, 3.8, 4.1)
             parameter_sets = []
             starting_coefficients = (0.04, 0.1, 0.3, 0.5, 1.0)
             ending_coefficients = (0.80, 2.0, 6.0, 10.0, 20.0)
-            sweep_training_parameters(
-                name_prefix="experiments/sae.shakespeare_64x4.regularized",
-                load_from=TrainingConfig.checkpoints_dir / "experiments/shakespeare_64x4.regularized",
-                starting_from=starting_coefficients,
-                ending_with=ending_coefficients,
-                steps=24,
+            num_sweeps = 5
+            for i in range(num_sweeps):
+                print(f"Starting parameter sweep {i}/{num_sweeps}")
+                sweep_training_parameters(
+                    name_prefix="experiments/regularization/sae.shakespeare_64x4.regularized",
+                    load_from=TrainingConfig.checkpoints_dir
+                    / "experiments/regularization/shakespeare_64x4.regularized",
+                    starting_from=starting_coefficients,
+                    ending_with=ending_coefficients,
+                    steps=24,
+                )
+
+        case 4:
+            """
+            Process CSV files and export results.json
+            """
+            base_dir = TrainingConfig.checkpoints_dir / "experiments/regularization"
+            column_names = ["layer", "coefficient", "l0", "ce_loss_increase"]
+            normal_csv = pd.read_csv(
+                base_dir / "sae.shakespeare_64x4.normal.csv",
+                header=None,
+                names=column_names,
             )
+            regularized_csv = pd.read_csv(
+                base_dir / "sae.shakespeare_64x4.regularized.csv",
+                header=None,
+                names=column_names,
+            )
+
+            data = {"original": [], "regularized": []}
+
+            for layer in range(5):
+                normal_data = normal_csv[normal_csv["layer"] == layer]
+                regularized_data = regularized_csv[regularized_csv["layer"] == layer]
+
+                # Group and average data by coefficient
+                normal_data = normal_data.groupby("coefficient").mean().reset_index()
+                regularized_data = regularized_data.groupby("coefficient").mean().reset_index()
+
+                # Sort data by L0
+                normal_data = normal_data.sort_values("l0")
+                regularized_data = regularized_data.sort_values("l0")
+
+                # Add data to dictionary
+                data["original"].append(
+                    [
+                        {
+                            "x": row.l0,
+                            "y": row.ce_loss_increase,
+                        }
+                        for row in normal_data.itertuples()
+                    ]
+                )
+                data["regularized"].append(
+                    [
+                        {
+                            "x": row.l0,
+                            "y": row.ce_loss_increase,
+                        }
+                        for row in regularized_data.itertuples()
+                    ]
+                )
+
+            with open(base_dir / "shakespeare_64x4.results.json", "w") as f:
+                json.dump(data, f, indent=4)
