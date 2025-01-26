dfb2b7d44fd63d5d861691148f567655f0c218b2
Peter Lai <peterjlai@gmail.com>
2025-01-26 08:59:24 -0800

    removing unnecessary metadata

--- config/__init__.py
+++ config/__init__.py
@@ -7,9 +7,9 @@ import torch
 
 @dataclass
 class Config:
-    name: str = field(metadata={"exclude": True}, default="")
-    device: torch.device = field(default_factory=lambda: get_default_device(), metadata={"exclude": True})
-    compile: bool = field(default_factory=lambda: get_default_device().type == "cuda", metadata={"exclude": True})
+    name: str = field(default="")
+    device: torch.device = field(default_factory=lambda: get_default_device())
+    compile: bool = field(default_factory=lambda: get_default_device().type == "cuda")
 
 
 @dataclass
