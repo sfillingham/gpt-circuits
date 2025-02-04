96bae66aca6cd15f84437fb4070a0bc5408026a7
Peter Lai <peterjlai@gmail.com>
2025-02-04 14:56:59 -0800

    progress on basic experiment

---
+++ experiments/circuits/__init__.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass
+from typing import Sequence
+
+import torch
+
+from models.sparsified import SparsifiedGPT
+
+
+@dataclass
+class MaskedFeature:
+    """
+    Represents a masked feature at a specific location.
+    """
+
+    token_idx: int
+    feature_idx: int
+
+
+@torch.no_grad()
+def calculate_kl_divergence(
+    model: SparsifiedGPT,
+    layer_idx: int,
+    target_token_idx: int,
+    target_logits: torch.Tensor,  # Shape: (V)
+    feature_magnitudes: torch.Tensor,  # Shape: (T, F)
+    masked_features: Sequence[MaskedFeature] = (),
+) -> tuple[float, dict[str, float]]:
+    """
+    Calculate KL divergence between target logits and logits produced by model using reconstructed activations.
+    """
+    # Copy feature magnitudes to avoid modifying original
+    feature_magnitudes = feature_magnitudes.clone()
+
+    # Ablate masked features
+    for masked_feature in masked_features:
+        feature_magnitudes[masked_feature.token_idx, masked_feature.feature_idx] = 0
+
+    # Reconstruct activations and compute logits
+    feature_magnitudes = feature_magnitudes.unsqueeze(0)  # Shape: (1, T, F)
+    x_reconstructed = model.saes[str(layer_idx)].decode(feature_magnitudes)  # type: ignore
+    predicted_logits = model.gpt.forward_with_patched_activations(x_reconstructed, layer_idx=layer_idx)
+    predicted_logits = predicted_logits.squeeze(0)[target_token_idx]  # Shape: (V)
+
+    # Compute KL divergence
+    kl_div = torch.nn.functional.kl_div(
+        torch.nn.functional.log_softmax(predicted_logits, dim=-1),
+        torch.nn.functional.softmax(target_logits, dim=-1),
+        reduction="sum",
+    )
+
+    # Calculate predictions
+    predictions = get_predictions(model, predicted_logits)
+
+    return kl_div.item(), predictions
+
+
+def get_predictions(
+    model: SparsifiedGPT,
+    logits: torch.Tensor,  # Shape: (V)
+    count: int = 5,
+) -> dict[str, float]:
+    """
+    Map logits to probabilities and return top 5 predictions.
+    """
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+    topk = torch.topk(probs, k=count)
+    results: dict[str, float] = {}
+    for i, p in zip(topk.indices, topk.values):
+        results[model.gpt.config.tokenizer.decode_token(int(i.item()))] = round(p.item() * 100, 2)
+    return results
--- experiments/circuits/basic.py
+++ experiments/circuits/basic.py
@@ -2,7 +2,7 @@
 Find all features needed to reconstruct the output logits of a model to within a certain KL divergence threshold using
 a brute-force technique.
 
-$ python -m experiments.circuits.basic --token_idx=0 --split=train --layer_idx=0
+$ python -m experiments.circuits.basic --sequence_idx=0 --token_idx=51 --layer_idx=0
 """
 
 import argparse
@@ -11,6 +11,7 @@ import torch
 
 from config import Config, TrainingConfig
 from data.dataloaders import DatasetShard
+from experiments.circuits import MaskedFeature, calculate_kl_divergence, get_predictions
 from models.sparsified import SparsifiedGPT, SparsifiedGPTOutput
 
 
@@ -19,7 +20,8 @@ def parse_args() -> argparse.Namespace:
     Parse command line arguments.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--token_idx", type=int, help="Starting index for token sequence to analyze")
+    parser.add_argument("--sequence_idx", type=int, help="Index for start of sequence [0...shard.tokens.size)")
+    parser.add_argument("--token_idx", type=int, help="Index for token in the sequence [0...block_size)")
     parser.add_argument("--shard_idx", type=int, default=0, help="Shard to load data from")
     parser.add_argument("--data_dir", type=str, default="data/shakespeare", help="Dataset split to use")
     parser.add_argument("--split", type=str, default="train", help="Dataset split to use")
@@ -27,29 +29,6 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--layer_idx", type=int, default=0, help="SAE layer to analyze")
     parser.add_argument("--threshold", type=float, default=1, help="Max threshold for KL divergence")
     return parser.parse_args()
-
-
-@torch.no_grad()
-def calculate_kl_divergence(
-    model: SparsifiedGPT,
-    target_logits: torch.Tensor,  # Shape: (T, V)
-    feature_magnitudes: torch.Tensor,  # Shape: (T, F)
-    layer_idx: int,
-) -> float:
-    """
-    Calculate KL divergence between target logits and logits produced by model using reconstructed activations.
-    """
-    feature_magnitudes = feature_magnitudes.unsqueeze(0)  # Shape: (1, T, F)
-    x_reconstructed = model.saes[str(layer_idx)].decode(feature_magnitudes)  # type: ignore
-
-    predicted_logits = model.gpt.forward_with_patched_activations(x_reconstructed, layer_idx=layer_idx)
-    predicted_logits = predicted_logits.squeeze(0)  # Shape: (T, V)
-    kl_div = torch.nn.functional.kl_div(
-        torch.nn.functional.log_softmax(predicted_logits, dim=-1),
-        torch.nn.functional.softmax(target_logits, dim=-1),
-        reduction="batchmean",
-    )
-    return kl_div.item()
 
 
 if __name__ == "__main__":
@@ -70,25 +49,54 @@ if __name__ == "__main__":
     shard = DatasetShard(dir_path=args.data_dir, split=args.split, shard_idx=args.shard_idx)
 
     # Get token sequence
-    tokens = shard.tokens[args.token_idx : args.token_idx + model.config.block_size].to(defaults.device)
+    layer_idx = args.layer_idx
+    target_token_idx = args.token_idx
+    tokens = shard.tokens[args.sequence_idx : args.sequence_idx + model.config.block_size].to(defaults.device)
     decoded_tokens = model.gpt.config.tokenizer.decode_sequence(tokens.tolist())
+    decoded_target = model.gpt.config.tokenizer.decode_token(int(tokens[target_token_idx].item()))
     print(f'Using sequence: "{decoded_tokens.replace("\n", "\\n")}"')
+    print(f"Target token: `{decoded_target}` at index {args.token_idx}")
+    print(f"Target layer: {layer_idx}")
 
     # Get target logits
     with torch.no_grad():
         output: SparsifiedGPTOutput = model(tokens.unsqueeze(0))
-    target_logits = output.logits.squeeze(0)
+    target_logits = output.logits.squeeze(0)[target_token_idx]  # Shape: (V)
+    target_predictions = get_predictions(model, target_logits)
+    print(f"Target predictions: {target_predictions}")
 
     # Get output for layer
-    layer_idx = args.layer_idx
-    feature_magnitudes = output.feature_magnitudes[layer_idx].squeeze(0)
-    x_reconstructed = output.reconstructed_activations[layer_idx].squeeze(0)
+    feature_magnitudes = output.feature_magnitudes[layer_idx].squeeze(0)  # Shape: (T, F)
 
     # Compute KL divergence if using all features
-    kl_div = calculate_kl_divergence(model, target_logits, feature_magnitudes, layer_idx)
-    print(f"KL divergence using all features for layer {layer_idx}: {round(kl_div, 4)}")
+    kl_div, predictions = calculate_kl_divergence(
+        model,
+        layer_idx,
+        target_token_idx,
+        target_logits,
+        feature_magnitudes,
+    )
+    print(f"Baseline KL div: {round(kl_div, 4)} - {predictions}")
 
-    # TODO: Find features needed to reconstruct logits
+    # Caculate ablation effects for each non-zero feature
+    feature_to_kl_div = {}
     non_zero_indices = torch.nonzero(feature_magnitudes, as_tuple=True)
     for token_idx, feature_idx in zip(*non_zero_indices):
-        pass
+        # Skip features for tokens that are after the target token
+        if token_idx <= target_token_idx:
+            masked_features = [MaskedFeature(token_idx=token_idx.item(), feature_idx=feature_idx.item())]
+            kl_div, predictions = calculate_kl_divergence(
+                model,
+                layer_idx,
+                target_token_idx,
+                target_logits,
+                feature_magnitudes,
+                masked_features,
+            )
+            feature_to_kl_div[(token_idx.item(), feature_idx.item())] = (kl_div, predictions)
+
+    # Sort features by KL divergence (descending)
+    print("\nFeatures sorted by KL divergence:")
+    sorted_features = sorted(feature_to_kl_div.items(), key=lambda x: x[1][0], reverse=True)
+    for (token_idx, feature_idx), (kl_div, predictions) in sorted_features:
+        print(f"({token_idx}, {feature_idx}): {round(kl_div, 4)} - {predictions}")
